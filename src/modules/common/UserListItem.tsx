c5893007e8357a11cbc460c026f66b2adbc10243
hiro <hiromhiro617@gmail.com>
2021-04-22 17:34:16 +0900

    UserListItemを修正

--- src/modules/common/UserListItem.tsx
+++ src/modules/common/UserListItem.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ListItem, Avatar, Icon, Button } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native';
+import firebase from 'firebase'
 
 const UserListItem: React.FC = (props) => {
+  const [imageSrc, setImageSrc] = useState(null);
   const navigation = useNavigation()
   const { user } = props
+
+  useEffect(() => {
+    getSource()
+  },[]);
+
+  const getAvatar =  () => {
+    return new Promise((resolve) => {
+      var storage = firebase.storage();
+      var storageRef = storage.ref();
+      var spaceRef = storageRef.child(`images/${user.uid}.jpg`);
+      spaceRef.getDownloadURL().then(function(url){
+        console.log("ファイルURLを取得")
+        console.log(url)
+        resolve(url);
+      }).catch(function(error) {
+        // Handle any errors
+        console.log("getTokoImage 画像を取得する");
+        console.log(error);
+      });
+    });
+  };
+
+  const getSource = () => {
+    getAvatar()
+    .then(res => {
+      setImageSrc(res)
+    })
+  }
   return (
       <ListItem
         onPress={() => navigation.push('UserPage', 
@@ -15,6 +45,9 @@ const UserListItem: React.FC = (props) => {
       >
         <Avatar
           title={user.name[0]}
+          source={{
+            uri: imageSrc
+          }}
           rounded
           containerStyle={{backgroundColor: 'gray'}}            
         />
