ce1253f7055ba193a09b1fbd8cd7beef726bca07
hiro <hiromhiro617@gmail.com>
2021-04-28 22:51:13 +0900

    Loginpageのデザインを修正

--- src/modules/Auth/LoginScreen.tsx
+++ src/modules/Auth/LoginScreen.tsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import {View, StyleSheet, Button,SafeAreaView } from 'react-native';
+import {ActivityIndicator,TouchableOpacity, Image, View, StyleSheet, Button,SafeAreaView } from 'react-native';
 import * as Google from 'expo-google-app-auth';
 import firebase from 'firebase';
-import { SocialIcon, Text } from 'react-native-elements'
+import {  SocialIcon, Text } from 'react-native-elements'
 
 class LoginScreen extends Component{
   constructor(props) {
     super(props);
-    this.state = {loading: false};
+    this.state = {loading: true};
   }
   isUserEqual = (googleUser, firebaseUser) => {
     if (firebaseUser) {
@@ -57,7 +57,7 @@ class LoginScreen extends Component{
 
   signInWithGoogleAsync = async () => {
     this.setState({
-      loading: true
+      loading: false
     })
     try {
       const result = await Google.logInAsync({
@@ -81,14 +81,47 @@ class LoginScreen extends Component{
   render() {
     return(
       <SafeAreaView style={styles.container}>
-        <Text h1 style={{fontWeight: 'bold', textAlign: 'center', marginBottom: 50}}>DONE HUB</Text>
-        <SocialIcon
-          title='Login With Google'
-          button
-          type='google'
-          loading={this.state.loading}
-          onPress={() => this.signInWithGoogleAsync()}
+        <Image
+          style={{
+            width: '100%',
+            height: undefined,
+            aspectRatio: 5 / 1,
+          }}
+          source={require('./images/DoneHub.png')}
         />
+        <Text
+          style={{paddingHorizontal: 65, paddingVertical: 50, textAlign: 'center', fontSize: 16, lineHeight: 22}}
+        >
+          利用規約とプライバシーポリシーに同意した上でログインしてください
+        </Text>
+        {this.state.loading &&
+        <TouchableOpacity
+          style={{
+            width: '100%', 
+            paddingHorizontal: 70, 
+            elevation: 2,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 1,
+            },
+            shadowOpacity: 0.20,
+            shadowRadius: 1.41,}}
+            onPress={() => this.signInWithGoogleAsync()}
+        >
+            <Image
+              style={{
+                width: '100%',
+                height: undefined,
+                aspectRatio: 5 / 1,
+              }}
+              source={require('./images/btn_google_signin.png')}
+            />
+        </TouchableOpacity>
+          }
+          {!this.state.loading && 
+            <ActivityIndicator size='large'/>
+          }
       </SafeAreaView>
     )
   }
@@ -100,6 +133,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingHorizontal: 10
+    alignItems: 'center',
   }
 })
